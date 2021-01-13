b9b80fc839ce2d4e67fd06f057a9711dd95f9087
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2021-01-13 16:16:29 +0300

    Remove old files

--- include/willow/check_words.h
+++
@@ -1,9 +0,0 @@
-#ifndef CHECK_WORDS_H_
-#define CHECK_WORDS_H_
-
-
-
-extern bool check_word(token * build_token);
-extern bool check_ident(token * build_token);
-
-#endif /* CHECK_WORDS_H_ */
--- include/willow/parse.h
+++
@@ -1,9 +0,0 @@
-#ifndef PARSE_H_
-#define PARSE_H_
-
-#include "type.h"
-
-parse(parser *parsing_ptr);
-void match(parser *parsing_ptr, token_type expected);
-
-#endif /* PARSE_H_ */
